6c7d8389fb8b93268fb7dc051a1f7b228ce7b3ad
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-08 22:11:37 +0200

    task-7

--- js/task-7.js
+++ js/task-7.js
@@ -5,13 +5,21 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 /*
 
+Задание
+Объяви переменную fruitsArrayLength и присвой ей длину массива fruits используя свойство length.
 
+Тесты
+Объявлена переменная fruitsArrayLength.
+Значение переменной fruitsArrayLength это число 4.
 
 */
 
 //Task START
 
-
+// Стартовый код
+const fruits = ['яблоко', 'персик', 'груша', 'банан'];
+// Пиши код ниже этой строки
+const fruitsArrayLength = fruits.length;
 
 //Task END
 
