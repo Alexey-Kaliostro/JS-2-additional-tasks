1441e2329c00df3edfb4eae243df80175ff4e1a8
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-11 23:31:07 +0200

    task-25-first-solution

--- js/task-25.js
+++ js/task-25.js
@@ -12,6 +12,36 @@ console.log(" ");
 
 //Task END
 
+
+/*
+first solution
+
+
+function getCommonElements(array1, array2) {
+  // Пиши код ниже этой строки
+const sameNumbers = [];
+let firstNumber;
+let secondNumber;
+
+for(let i = 0; i < array1.length; i++){
+    firstNumber = array1[i];
+    
+    for(let j = 0; j < array2.length; j++){
+        secondNumber = array2[j]
+        
+        if(firstNumber === secondNumber){
+            sameNumbers.push(firstNumber);
+    
+        }
+   
+    }
+
+}
+  return sameNumbers;
+  // Пиши код выше этой строки
+}
+*/
+
 console.log(" ");
 console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 
